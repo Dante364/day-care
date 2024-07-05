5feaa5dcb37c25031b6dcf15fb8e06c30e7ba6cc
Daniel Nhial <daniel.nhial@strathmore.edu>
2024-07-05 13:25:57 +0300

    Feat: Chart Livewire components

---
+++ app/Livewire/Chart/Staff/Bar.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Chart\Staff;
+
+use Livewire\Component;
+
+class Bar extends Component
+{
+    public function render()
+    {
+        return view('livewire.chart.staff.bar');
+    }
+}
